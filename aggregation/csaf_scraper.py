4c31f4db4269ad3fa181d17ffdc9795422f635a1
Israel Bentley <israel.bentley@icloud.com>
2025-01-14 13:25:58 -0700

    initial commit

---
+++ aggregation/csaf_scraper.py
@@ -0,0 +1,42 @@
+import requests
+import json
+import os 
+
+from rolie_config import rolie_feeds
+
+for rolie_name, rolie_url in rolie_feeds.items():
+    try:
+        print(rolie_url)
+        rolie_response = requests.get(
+            rolie_url, allow_redirects=True, verify=True
+        )
+        rolie = rolie_response.json()["feed"]
+        if rolie:
+            if rolie["title"]:
+                if not os.path.exists("../"+rolie["title"]):
+                    os.makedirs("../"+rolie["title"])
+            else:
+                print("ROLIE missing critical information")
+            if rolie["entry"]["content"]["src"]:
+                for entry in rolie["entry"]:
+                    csaf_response = requests.get(entry["content"]["src"])
+                    csaf = csaf_response.json()
+                    if csaf:
+                        with open(f"../{rolie['title']}/{entry['id']}.json", "w") as outfile: # Should there be additional folders by publication year?
+                            json.dump(csaf, outfile, indent=2, sort_keys=True)
+            else:
+                print("ROLIE missing critical information")
+    except Exception as e:
+        print(e)
+
+
+
+
+
+
+        
+
+
+
+    
+ 
---
+++ aggregation/rolie_config.py
@@ -0,0 +1,4 @@
+rolie_feeds = {
+    "cisa-csaf-ot-feed-tlp-white": "https://raw.githubusercontent.com/cisagov/CSAF/develop/csaf_files/OT/white/cisa-csaf-ot-feed-tlp-white.json",
+    "nn-csaf-feed-tlp-white": "https://security.nozominetworks.com/csaf/feed-tlp-white.json",
+}

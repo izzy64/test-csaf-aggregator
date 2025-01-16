015c395f1b46118d69b8774e168fea7e27b9a594
Israel Bentley <israel.bentley@icloud.com>
2025-01-16 11:12:10 -0700

    testing

--- aggregation/csaf_scraper.py
+++ aggregation/csaf_scraper.py
@@ -18,12 +18,14 @@ for rolie_name, rolie_url in rolie_feeds.items():
             else:
                 print("ROLIE missing critical information")
             if rolie["entry"]:
-                for entry in rolie["entry"]:
-                    csaf_response = requests.get(entry["content"]["src"])
-                    csaf = csaf_response.json()
-                    if csaf:
-                        with open(f"../{rolie['title']}/{entry['id']}.json", "w") as outfile: # Should there be additional folders by publication year?
-                            json.dump(csaf, outfile, indent=2, sort_keys=True)
+                with open(f"../{rolie['title']}/{rolie['id']}.json", "w") as outfile: # Should there be additional folders by publication year?
+                    json.dump(rolie, outfile, indent=2, sort_keys=True)
+                # for entry in rolie["entry"]:
+                #     csaf_response = requests.get(entry["content"]["src"])
+                #     csaf = csaf_response.json()
+                #     if csaf:
+                #         with open(f"../{rolie['title']}/{entry['id']}.json", "w") as outfile: # Should there be additional folders by publication year?
+                #             json.dump(csaf, outfile, indent=2, sort_keys=True)
             else:
                 print("ROLIE missing critical information")
     except Exception as e:
